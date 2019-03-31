9735b394016fea7a66108316dd9106e1bb8d1169
Andrew <andrewpeliza@hotmail.com>
2019-03-31 15:43:52 +0100

    Webpack now working as expected, need to work with the config file next.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('Hello From Webpack');
+import { run } from './app/app';
+import { AlertService } from './app/alert.service';
+import { ComponetService } from './app/component.service';
+
+const alertService = new AlertService();
+const componentService = new ComponetService();
+
+run(alertService, componentService);
