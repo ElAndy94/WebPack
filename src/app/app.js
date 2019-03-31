252004640f1cbc4847105362dafb4260a2e350c8
Andrew <andrewpeliza@hotmail.com>
2019-03-31 15:20:18 +0100

    Folder structure set, code separated. Next webpack config/ import/export

---
+++ src/app/app.js
@@ -0,0 +1,19 @@
+const alertService = new AlertService();
+const componentService = new ComponetService();
+const run = (alertService, componentService) => {
+  alertService.hideErrors();
+
+  componentService.onClick(() => {
+    alertService.hideErrors();
+    const inputs = [numberOneInput.value, numberTwoInput.value];
+    const parsedInputs = parseInputs(...inputs);
+    if (inputsAreValid(...parsedInputs)) {
+      const [numA, numB] = parsedInputs;
+      resultDiv.innerText = numA + numB;
+    } else {
+      resultDiv.innerText = '';
+      handleAdditionError(inputs, parsedInputs);
+    }
+  });
+};
+run(alertService, componentService);
