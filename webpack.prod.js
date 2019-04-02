42eec92dbe2df2cd2b9d3813743b5073d2d56a85
Andrew <andrewpeliza@hotmail.com>
2019-04-02 09:41:29 +0100

    Html-loader, File Loader and Clean Webpack have been added and working.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -10,7 +11,8 @@ module.exports = merge(common, {
   output: {
     filename: 'main.[contentHash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new CleanWebpackPlugin()]
   //   plugins: [
   //     new HtmlWebpackPlugin({
   //       template: './src/template.html'
