ec5cad732ff57c848d8316387374cce72f07f3d5
Andrew <andrewpeliza@hotmail.com>
2019-04-02 11:52:43 +0100

    Webpack finished, minifying the css + html.

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   // mode: 'development',
-  //   devtool: 'none',
+  // devtool: 'none',
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js'
   },
-  // output: {
-  //   filename: 'main.[contentHash].js',
-  //   path: path.resolve(__dirname, 'dist')
-  // },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', //3. Inject styles into DOM
-          'css-loader', //2. Turns css into commonjs
-          'sass-loader' //1. Turns sass into css
-        ]
-      },
       {
         test: /\.html$/,
         use: ['html-loader']
